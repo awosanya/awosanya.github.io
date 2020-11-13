6565d3003fad7c1fce91612ef0fd0b62280b1334
Oladimeji Awosanya <awosanya.oladimeji@gmail.com>
2020-11-13 13:18:10 +0100

    Add files via upload

---
+++ scripts/resumePage.js
@@ -0,0 +1,8 @@
+function resumePages()
+	{
+	document.getElementById("core_competences").style.display="block";
+	document.getElementById("skillandTools").style.display="block";	
+	document.getElementById("experiences").style.display="block";	
+	document.getElementById("educations").style.display="block";
+	document.getElementById("leadershipANDTraining").style.display="block";
+	}
